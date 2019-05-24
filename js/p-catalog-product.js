75c5d9892ccbc345e1eaf751bf7fdb7b1bfac8c0
Max Shevkoplias <shevkoplias.mk@yandex.ru>
2019-05-24 18:03:23 +0300

    Страница: Контентная страница

--- js/p-catalog-product.js
+++ js/p-catalog-product.js
@@ -63,31 +63,4 @@ $(function() {
 	});
 
 	var sliderPopup = window.initSliderPopup('b-photo-slider');
-	
-	// Масонри на гридах
-	function resizeGridItem($item) {
-		var $grid = $('.product-specifications__grid');
-		rowHeight = parseInt($grid.css('grid-auto-rows'));
-		rowGap = parseInt($grid.css('grid-row-gap'));
-		rowSpan = Math.ceil(($item.children()[0].getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-		$item[0].style.gridRowEnd = "span "+rowSpan;
-	}
-
-	function resizeAllGridItems() {
-		$(".product-specifications__col").each(function(id, el) {
-			resizeGridItem($(el));
-		});
-	}
-
-	resizeAllGridItems();
-	$(window).ready(function() {
-		resizeAllGridItems();
-	});
-	$(window).on('resize', function() {
-		resizeAllGridItems();
-	});
-
-	$('.b-accordion').on('toggle', function() {
-		resizeAllGridItems();
-	});
 });
